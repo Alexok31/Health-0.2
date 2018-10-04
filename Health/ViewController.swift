07da58dbc8c2e1a6672c58823798930008f741fd
Александр Харченко <kharchenkoau31@gmail.com>
2018-10-04 17:06:26 +0300

    Initial Commit

---
+++ Health/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Health
+//
+//  Created by Александр Харченко on 10/4/18.
+//  Copyright © 2018 Александр Харченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
